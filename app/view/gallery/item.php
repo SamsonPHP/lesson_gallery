307d0ede5d0c314bd2083eb3450d2cbc32026b00
zyablitskiy <zyablitskiy@gmail.com>
2015-02-19 18:09:37 +0200

    show date and size

--- app/view/gallery/item.php
+++ app/view/gallery/item.php
@@ -3,4 +3,6 @@
     <img src="<?php iv('image_Src')?>"  title="<?php iv('image_Name')?>">
     <a class="btn edit" href="<?php  url_base('gallery', 'form', 'image_PhotoID')?>">Редактировать</a>
     <a class="btn delete" href="<?php url_base('gallery', 'delete', 'image_PhotoID')?>">Удалить</a>
+    <span><?php iv('image_Loaded') ?></span>
+    <span><?php iv('image_size') ?></span>
 </li>
